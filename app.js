78dd227df47bc0c18ca06040851c303c77326216
Julia Hocken <ju.hocken@gmail.com>
2024-09-12 17:13:54 -0400

    format for button selection in place, missing html

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const inputBox = document.querySelector("input");
 
+addEventListener("click", function (e) {
+  e.preventDefault();
+  //   console.log(e.target);
+  //   console.log(e.target.dataset.seven);
+  console.log(e.target.dataset[e.target.id]);
+});
+
+//targets button 7
+const btn7 = document.getElementById("seven");
+const btn7Val = +btn7.dataset.seven;
+
+// console.log(btn7Val);
+
+// console.log(typeof btn7Val);
